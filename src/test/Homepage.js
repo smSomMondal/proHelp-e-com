5225d9b8efa6b2c25262d43fae5c196a9ee388dc
anirbanghorai <anirbanghorai107@gmail.com>
2025-04-08 18:50:43 +0530

    rrr

---
+++ src/test/Homepage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Homepage() {
+  return (
+    <div>
+        <h1>Contact</h1>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Reprehenderit delectus eius maiores fugit magni, iusto expedita provident a harum laborum natus quia voluptatem dolore nobis quae, et, pariatur iure tempora?
+    </div>
+  )
+}
+
+export default Homepage
