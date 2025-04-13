2cd45c92694a00b7a39783abbab84e53c3baaa68
Som Mondal <sommondal5872@gmail.com>
2025-04-14 01:24:45 +0530

    add login and signup 14/04/2025

--- src/App.js
+++ src/App.js
@@ -1,20 +1,27 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import ProductPage from './components/ProductPage';
-import AllProducts from './components/AllProducts';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+// import ProductPage from './components/ProductPage';
+// import AllProducts from './components/AllProducts';
+import Login from './components/Login';
+import Signup from './components/Signup';
 
 
 function App() {
   return (
-    <Router>
+    <>
       <div className="App">
-        <Routes>
-          
-          <Route path="/products" element={<ProductPage />} />
-          <Route path="/all-products" element={<AllProducts />} />
-        </Routes>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<Signup />} />
+            {/* <Route path="/products" element={<ProductPage />} />
+          <Route path="/all-products" element={<AllProducts />} /> */}
+          </Routes>
+
+        </BrowserRouter>
       </div>
-    </Router>
+    </>
+
   );
 }
 
