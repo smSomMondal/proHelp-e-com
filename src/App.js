12026cbbe431486a9155c83492af185a2528aea6
Samprit <sampritroy99@gmail.com>
2025-04-15 13:59:33 +0530

    update error correction 15/04/2025

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from "react";
-import { BrowserRouter, Route, Routes, Link } from "react-router-dom";
+// import { BrowserRouter, Route, Routes, Link } from "react-router-dom";
 import Home from './test/Home';
 import Contact from './test/Contact';
 import About from './test/About';
 import ProductList from "./components/ProductList";
-import React from 'react';
+// import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 // import ProductPage from './components/ProductPage';
 // import AllProducts from './components/AllProducts';
@@ -22,28 +22,22 @@ function App() {
   };
 
   return (
-    <BrowserRouter>
-      <div>
+    <>
+      <div className="App">
+        <BrowserRouter>
         <nav style={{ marginBottom: "20px" }}>
           <Link to="/">Home</Link> |{" "}
           <Link to="/con">Contact</Link> |{" "}
           <Link to="/abo">About</Link> |{" "}
           <Link to="/products">Product List</Link>
         </nav>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/con" element={<Contact />} />
-          <Route path="/abo" element={<About userInfo={user} />} />
-          <Route path="/products" element={<ProductList />} />
-        </Routes>
-      </div>
-    </BrowserRouter>
-    <>
-      <div className="App">
-        <BrowserRouter>
           <Routes>
             <Route path="/login" element={<Login />} />
             <Route path="/signup" element={<Signup />} />
+            <Route path="/" element={<Home />} />
+          <Route path="/con" element={<Contact />} />
+          <Route path="/abo" element={<About userInfo={user} />} />
+          <Route path="/products" element={<ProductList />} />
             {/* <Route path="/products" element={<ProductPage />} />
           <Route path="/all-products" element={<AllProducts />} /> */}
           </Routes>
