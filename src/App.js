319020ee6f711bb44c62486f4cd5501c8cdb081f
Som Mondal <sommondal5872@gmail.com>
2025-04-15 15:26:25 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import React from "react";
-// import { BrowserRouter, Route, Routes, Link } from "react-router-dom";
+import { BrowserRouter, Route, Routes, Link } from "react-router-dom";
 import Home from './test/Home';
 import Contact from './test/Contact';
 import About from './test/About';
 import ProductList from "./components/ProductList";
-// import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
 // import ProductPage from './components/ProductPage';
 // import AllProducts from './components/AllProducts';
 import Login from './components/Login';
@@ -25,19 +23,19 @@ function App() {
     <>
       <div className="App">
         <BrowserRouter>
-        <nav style={{ marginBottom: "20px" }}>
-          <Link to="/">Home</Link> |{" "}
-          <Link to="/con">Contact</Link> |{" "}
-          <Link to="/abo">About</Link> |{" "}
-          <Link to="/products">Product List</Link>
-        </nav>
+          <nav style={{ marginBottom: "20px" }}>
+            <Link to="/">Home</Link> |{" "}
+            <Link to="/con">Contact</Link> |{" "}
+            <Link to="/abo">About</Link> |{" "}
+            <Link to="/products">Product List</Link>
+          </nav>
           <Routes>
             <Route path="/login" element={<Login />} />
             <Route path="/signup" element={<Signup />} />
             <Route path="/" element={<Home />} />
-          <Route path="/con" element={<Contact />} />
-          <Route path="/abo" element={<About userInfo={user} />} />
-          <Route path="/products" element={<ProductList />} />
+            <Route path="/con" element={<Contact />} />
+            <Route path="/abo" element={<About userInfo={user} />} />
+            <Route path="/products" element={<ProductList />} />
             {/* <Route path="/products" element={<ProductPage />} />
           <Route path="/all-products" element={<AllProducts />} /> */}
           </Routes>
