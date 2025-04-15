d6e07f4e4ef4d730fa2d32060b3fc2362c62bbc5
anirbanghorai <anirbanghorai107@gmail.com>
2025-04-15 20:22:05 +0530

    done

--- backend/route/productRoute.js
+++ backend/route/productRoute.js
@@ -6,3 +6,13 @@ const productApi = express.Router();
 productApi.post("/addProduct", addProduct);
 
 export default productApi;
+/*
+const express = require("express");
+const { createProduct, getProducts } = require("../controller/productController");
+const router = express.Router();
+
+router.post("/add", createProduct);
+router.get("/", getProducts);
+
+module.exports = router;
+*/
