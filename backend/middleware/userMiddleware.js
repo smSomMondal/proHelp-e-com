1125a96a52d93d1e88da7bfc83c4006b11aea601
Som Mondal <sommondal5872@gmail.com>
2025-04-17 21:22:45 +0530

    update controlar 17/04/2025

--- backend/middleware/userMiddleware.js
+++ backend/middleware/userMiddleware.js
@@ -30,4 +30,40 @@ const chqProtectedUser = expressAsyncHandler(async (req, res, next) => {
     }
 
 });
-export { chqProtectedUser };
+
+const chqSeler = expressAsyncHandler(async (req, res, next) => {
+    try {
+        let token;
+
+        if (req.headers.authorization && req.headers.authorization.startsWith("Bearer")) {
+            try {
+                //req.headers.authorization="Bearer" "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY2OTE2ZDYzM2YzZDI1MjcyNzAxODk0YyIsImlhdCI6MTcyMDgwNjg3NCwiZXhwIjoxNzIzMzk4ODc0fQ.EnYqKS5FK3F6Pb81P5kyGHWdCeKJSxP_9TiimzORt7Q"
+                token = req.headers.authorization.split(" ")[1];
+                const decoded = jwt.verify(token, process.env.JWT_SECRET);
+                let user = await User.findById(decoded.id).select("-password");
+                // Check if the user is a seller
+                if (user && user.role === "seller") {
+                    req.user = user;
+                } else {
+                    res.status(401);
+                    throw new Error("Not authorized as a seller");
+                }
+                next();
+            }
+            catch (err) {
+                res.status(401);
+                throw new Error("Not authorized, token failed");
+            }
+        }
+        if (!token) {
+            res.status(401);
+            throw new Error("Not authorized, no token");
+        }
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ message: "Server error" });
+
+    }
+
+});
+export { chqProtectedUser,chqSeler };
--- backend/route/productRoute.js
+++ backend/route/productRoute.js
@@ -1,8 +1,11 @@
 import express from "express";
-import { addProduct } from "../controller/productController.js";
+import { addProduct, deleteProduct, updateProduct } from "../controller/productController.js";
+import { chqSeler } from "../middleware/userMiddleware.js";
 
 const productApi = express.Router();
 
-productApi.post("/addProduct", addProduct);
+productApi.post("/addProduct",chqSeler,addProduct);
+productApi.post("/updateProduct",chqSeler, updateProduct);
+productApi.post("/deleteProduct",chqSeler, deleteProduct);
 
 export default productApi;
